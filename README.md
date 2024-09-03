cc5937f39910994a9a2f3bbb7f492b4cd666d69e
mohamed khial <126480908+mokhial27@users.noreply.github.com>
2024-09-03 14:34:41 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+"# weather-app-" 
+"# weather-app-" 
+"# weather-app-" 
+"# weather-app-" 
---
+++ server.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const axios = require("axios");
+const app = express();
+require('dotenv').config();
+
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.render("index", { weather: null, error: null });
+});
+
+app.get("/weather", async (req, res) => {
+  const city = req.query.city;
+  const apiKey = '';
+
+  const APIUrl = ``;
+  let weather;
+  let error = null;
+
+  try {
+    const response = await axios.get(APIUrl);
+    weather = response.data;
+  } catch (error) {
+    weather = null;
+    error = "Error, Please try again";
+  }
+  res.render("index", { weather, error });
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`App is running on port ${port}`);
+});
